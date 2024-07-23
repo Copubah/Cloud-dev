db07e57503e1fb7e2f913ab6fa2c5de831e0b975
Cloudev <copubah@gmail.com>
2024-07-23 18:55:33 +0300

    hotfix

--- app.js
+++ app.js
@@ -7,4 +7,16 @@ menuIcon.onclick = () => {
 }
 
 
-let sections = document.querySelector
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height)
+    })
+}
